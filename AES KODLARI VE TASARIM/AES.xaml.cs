025be803c92671641a20ff3f87d57fead862a24c
Furkan Demirel <46024787+FurkansPLUGIN@users.noreply.github.com>
2021-11-06 23:45:49 +0300

    Add files via upload

---
+++ AES KODLARI VE TASARIM/AES.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using PCLCrypto;
+
+namespace Asama1.AnaSayfalar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AES : ContentPage
+    {
+        public byte[] Salt { get; set; }
+        public string TextToBeDecrypted { get; set; }
+
+        public AES()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void sifrele_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(metin.Text))
+            {
+                Salt = AESHelp.CreateSalt(16);
+                TextToBeDecrypted = AESHelp.sifrele(metin.Text, Salt);
+                SifreliMetin.Text = TextToBeDecrypted;
+            }
+        }
+
+
+      
+
+        private void coz_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextToBeDecrypted))
+            {
+                ÇözülmüşMetin.Text = AESHelp.SifreyiCoz(TextToBeDecrypted, Salt);
+            }
+        }
+    }
+}
---
+++ AES KODLARI VE TASARIM/AESHelp.cs
@@ -0,0 +1,54 @@
+﻿using PCLCrypto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asama1.AnaSayfalar
+{
+   public class AESHelp
+    {
+        public static byte[] CreateSalt(int byteUzunlugu)
+        {
+            return WinRTCrypto.CryptographicBuffer.GenerateRandom(byteUzunlugu);
+        }
+        const string KEYDEGERLERI = "AFRGSHgehsheyikvs";
+        public static byte[] CreateDerivedKey(string sifre, byte[] salt, int anahtarUzunlugu = 32, int tekrar = 1000)
+        {
+            byte[] anatar = NetFxCrypto.DeriveBytes.GetBytes(sifre, salt, tekrar, anahtarUzunlugu);
+            return anatar;
+        }
+
+        public static string sifrele(string veri, byte[] salt)
+        {
+            if (string.IsNullOrEmpty(veri))
+            {
+                return null;
+            }
+
+            byte[] anahtar = CreateDerivedKey(KEYDEGERLERI, salt);
+
+            ISymmetricKeyAlgorithmProvider aes = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(PCLCrypto.SymmetricAlgorithm.AesCbcPkcs7);
+            ICryptographicKey symetricKey = aes.CreateSymmetricKey(anahtar);
+            var bytes = WinRTCrypto.CryptographicEngine.Encrypt(symetricKey, Encoding.UTF8.GetBytes(veri));
+            var sifrelenmisMetin = Convert.ToBase64String(bytes);
+            return sifrelenmisMetin;
+        }
+
+
+        public static string SifreyiCoz(string veri, byte[] salt)
+        {
+            if (string.IsNullOrEmpty(veri))
+            {
+                return null;
+            }
+
+            byte[] anahtar = CreateDerivedKey(KEYDEGERLERI, salt);
+
+            ISymmetricKeyAlgorithmProvider aes = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(PCLCrypto.SymmetricAlgorithm.AesCbcPkcs7);
+            ICryptographicKey simetrikAnahtar = aes.CreateSymmetricKey(anahtar);
+            var sifrelenmisMetin = Convert.FromBase64String(veri);
+            var bytes = WinRTCrypto.CryptographicEngine.Decrypt(simetrikAnahtar, sifrelenmisMetin);
+            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+        }
+    }
+}
